a29e6e23350496b3948d5e1bb483d0360aa9c399
Lucas Frias <lucasfrias@mailfence.com>
2026-02-19 20:58:47 -0600

    Created mock GUI

--- main.py
+++ main.py
@@ -9,13 +9,35 @@
 # MODULES #
 ###########
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QPushButton
 
-app = QApplication(sys.argv)
 
-window = QWidget()
-window.setWindowTitle("My First PySide6 App")
-window.resize(400, 300)
-window.show()
 
-sys.exit(app.exec())
+class Window:
+    def __init__(self) -> None:
+        """Creates a Window object"""
+        self.app = QApplication(sys.argv)
+
+        #define window parent behavior
+        self.window = QWidget()
+        self.window.setWindowTitle("The Count! for MIPS")
+        self.window.show()
+
+        #input box
+        self.mipsInputBox = QTextEdit()
+        self.mipsInputBox.setPlaceholderText("Put MIPS here!")
+
+        #convert button
+        self.convertButton = QPushButton("Convert to hex!")
+        #input box
+        self.hexOutputBox = QTextEdit()
+        self.hexOutputBox.setPlaceholderText("Output will be here...")
+
+        #Add GUI elements
+        layout = QVBoxLayout()
+        layout.addWidget(self.mipsInputBox)
+        layout.addWidget(self.convertButton)
+        layout.addWidget(self.hexOutputBox)
+        self.window.setLayout(layout)
+        sys.exit(self.app.exec())
+Window()
